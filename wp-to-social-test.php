7a2958ab25b9f38d91124caa6c32de6086ca2705
DarkVapor <30492361+DarkVapor@users.noreply.github.com>
2023-04-21 15:00:27 +0200

    Change module name
    Change module name

--- wp-to-social-test.php
+++ wp-to-social-test.php
@@ -145,3 +145,108 @@ function verifier_champs_personnalises_avant_publication() {
 }
 
 add_action('admin_head', 'hide_publish_button');
+
+function wp2social_configuration_page() {
+    ?>
+    <div class="wrap">
+        <h1>WP2Social Settings</h1>
+        <form method="post" action="options.php">
+            <?php
+                settings_fields( 'configuration_group' );
+                do_settings_sections( 'wp2social_options_page' );
+                submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+function wp2social_field_tw_account_id() {
+    $tw_account_id = get_option( 'tw_account_id' );
+    ?>
+    <input type="text" name="tw_account_id" style="width: 100%" value="<?php echo esc_attr( $tw_account_id ); ?>">
+    <?php
+}
+
+function wp2social_field_tw_consumer_key() {
+    $tw_consumer_key = get_option( 'tw_consumer_key' );
+    ?>
+    <input type="text" name="tw_consumer_key" style="width: 100%" value="<?php echo esc_attr( $tw_consumer_key ); ?>">
+    <?php
+}
+
+function wp2social_field_tw_consumer_secret() {
+    $tw_consumer_secret = get_option( 'tw_consumer_secret' );
+    ?>
+    <input type="text" name="tw_consumer_secret" style="width: 100%" value="<?php echo esc_attr( $tw_consumer_secret ); ?>">
+    <?php
+}
+
+function wp2social_field_tw_bearer_token() {
+    $tw_bearer_token = get_option( 'tw_bearer_token' );
+    ?>
+    <input type="text" name="tw_bearer_token" style="width: 100%" value="<?php echo esc_attr( $tw_bearer_token ); ?>">
+    <?php
+}
+
+function wp2social_field_tw_access_token() {
+    $tw_access_token = get_option( 'tw_access_token' );
+    ?>
+    <input type="text" name="tw_access_token" style="width: 100%" value="<?php echo esc_attr( $tw_access_token ); ?>">
+    <?php
+}
+
+function wp2social_field_tw_access_token_secret() {
+    $tw_access_token_secret = get_option( 'tw_access_token_secret' );
+    ?>
+    <input type="text" name="tw_access_token_secret" style="width: 100%" value="<?php echo esc_attr( $tw_access_token_secret ); ?>">
+    <?php
+}
+
+function wp2social_field_fb_access_token() {
+    $fb_access_token = get_option( 'fb_access_token' );
+    ?>
+    <input type="text" name="fb_access_token" style="width: 100%" value="<?php echo esc_attr( $fb_access_token ); ?>">
+    <?php
+}
+
+function wp2social_field_dc_webhook_url() {
+    $dc_webhook_url = get_option( 'dc_webhook_url' );
+    ?>
+    <input type="text" name="dc_webhook_url" style="width: 100%" value="<?php echo esc_attr( $dc_webhook_url ); ?>">
+    <?php
+}
+
+
+function wp2social_settings() {
+    register_setting( 'configuration_group', 'tw_account_id' );
+    register_setting( 'configuration_group', 'tw_consumer_key' );
+    register_setting( 'configuration_group', 'tw_consumer_secret' );
+    register_setting( 'configuration_group', 'tw_bearer_token' );
+    register_setting( 'configuration_group', 'tw_access_token' );
+    register_setting( 'configuration_group', 'tw_access_token_secret' );
+    register_setting( 'configuration_group', 'fb_access_token' );
+    register_setting( 'configuration_group', 'dc_webhook_url' );
+
+    add_settings_section( 'wp2social_section_twitter', 'Twitter Settings', '', 'wp2social_options_page' );
+    add_settings_field( 'wp2social_field_tw_account_id', 'Twitter Account ID', 'wp2social_field_tw_account_id', 'wp2social_options_page', 'wp2social_section_twitter' );
+    add_settings_field( 'wp2social_field_tw_consumer_key', 'Twitter Consumer Key', 'wp2social_field_tw_consumer_key', 'wp2social_options_page', 'wp2social_section_twitter' );
+    add_settings_field( 'wp2social_field_tw_consumer_secret', 'Twitter Consumer Secret', 'wp2social_field_tw_consumer_secret', 'wp2social_options_page', 'wp2social_section_twitter' );
+    add_settings_field( 'wp2social_field_tw_bearer_token', 'Twitter Bearer Token', 'wp2social_field_tw_bearer_token', 'wp2social_options_page', 'wp2social_section_twitter' );
+    add_settings_field( 'wp2social_field_tw_access_token', 'Twitter Access Token', 'wp2social_field_tw_access_token', 'wp2social_options_page', 'wp2social_section_twitter' );
+    add_settings_field( 'wp2social_field_tw_access_token_secret', 'Twitter Access Token Secret', 'wp2social_field_tw_access_token_secret', 'wp2social_options_page', 'wp2social_section_twitter' );
+
+    add_settings_section( 'wp2social_section_facebook', 'Facebook Settings', '', 'wp2social_options_page' );
+    add_settings_field( 'wp2social_field_fb_access_token', 'Facebook Access Token', 'wp2social_field_fb_access_token', 'wp2social_options_page', 'wp2social_section_facebook' );
+
+    add_settings_section( 'wp2social_section_discord', 'Discord Settings', '', 'wp2social_options_page' );
+    add_settings_field( 'wp2social_field_dc_webhook_url', 'Discord Webhook URL', 'wp2social_field_dc_webhook_url', 'wp2social_options_page', 'wp2social_section_discord' );
+}
+
+
+add_action( 'admin_init', 'wp2social_settings' );
+add_action( 'admin_menu', 'wp2social_add_admin_menu' );
+
+function wp2social_add_admin_menu() {
+    add_menu_page( 'WP2Social', 'WP2Social', 'manage_options', 'wp2social', 'wp2social_configuration_page' );
+}
